d126ba8a42051bb87882ff8fa0c1372b6c987c82
morrismag <morrissmag@gmail.com>
2023-05-11 13:45:01 +0300

    KS-1-2 dismissal of staff

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main() {
+    val numberOFOrders: Int = 75
+    val titleGratitude: String = "Вы лучшие покупатели"
+
+        println("Общее кол-во заказов = $numberOFOrders")
+        println("Благодарность: \"$titleGratitude\"")
+        println()
+
+    val storeStaff: Int = 2000
+
+        //println("Персонал магазина составляет $storeStaff человек")
+
+    val storeStaffAfterParty: Int = storeStaff - 1
+        println("$storeStaff негритят отправились обедать,\n" +
+                "Один поперхнулся, их осталось $storeStaffAfterParty.")
+
+}
