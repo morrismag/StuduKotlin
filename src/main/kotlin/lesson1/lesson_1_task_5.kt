e1d703527617949d3ebcfc5b81c317deb6af1254
morrismag <morrissmag@gmail.com>
2023-05-12 05:56:35 +0300

    KS-1-5 In space in minutes

--- src/main/kotlin/lesson1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson1/lesson_1_task_5.kt
@@ -1,14 +1,14 @@
 package lesson1
 
-fun main(){
+fun main() {
     val secondsOfFirstFlight: Short = 6480
     var minutesOfFirstFlight: Int = 0
-    var remainsSecond: Int =0
+    var remainsSecond: Int = 0
 
-    minutesOfFirstFlight = secondsOfFirstFlight/60
-    remainsSecond = secondsOfFirstFlight - minutesOfFirstFlight*60
+    minutesOfFirstFlight = secondsOfFirstFlight / 60
+    remainsSecond = secondsOfFirstFlight - minutesOfFirstFlight * 60
 
-    if (remainsSecond in 0..9){
+    if (remainsSecond in 0..9) {
         println("Гагарин провел в космосе: $minutesOfFirstFlight:0$remainsSecond  в минутах")
     } else {
         println("Гагарин провел в космосе: $minutesOfFirstFlight:$remainsSecond в минутах")
