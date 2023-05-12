d7bca293a9a1a268b97c377514463f8f00127cf8
morrismag <morrissmag@gmail.com>
2023-05-12 05:47:08 +0300

    KS-2-2 Workers vs Interns

--- src/main/kotlin/lesson2/lesson_2_task_2.kt
+++ src/main/kotlin/lesson2/lesson_2_task_2.kt
@@ -1,4 +1,5 @@
 package lesson2
+
 // ... работа не волк, в лес не убежит ...
 fun main() {
     val oldWorkers = 50
@@ -6,14 +7,20 @@ fun main() {
     val newInterns = 30
     val salaryNewInterns = 20000
 
-    val sumSalaryOldWorkers = oldWorkers*salaryOldWorkers
-    val sumSalaryAll = (oldWorkers*salaryOldWorkers)+(newInterns*salaryNewInterns)
-    val averangeSalary: Int = sumSalaryAll/(oldWorkers+newInterns)
+    val sumSalaryOldWorkers = oldWorkers * salaryOldWorkers
+    val sumSalaryAll = (oldWorkers * salaryOldWorkers) + (newInterns * salaryNewInterns)
+    val averangeSalary: Int = sumSalaryAll / (oldWorkers + newInterns)
 
-    println("Расходы на выплату заплаты постоянных сотрудников составляет:\n" +
-            "$sumSalaryOldWorkers рублей")
-    println("Общие расходы по зарплате после устройства стажеров составляет:\n" +
-            "$sumSalaryAll рублей")
-    println("Средняя зарплата сотрудника после устройства стажеров составила:\n" +
-            "$averangeSalary рублей")
+    println(
+        "Расходы на выплату заплаты постоянных сотрудников составляет:\n" +
+                "$sumSalaryOldWorkers рублей"
+    )
+    println(
+        "Общие расходы по зарплате после устройства стажеров составляет:\n" +
+                "$sumSalaryAll рублей"
+    )
+    println(
+        "Средняя зарплата сотрудника после устройства стажеров составила:\n" +
+                "$averangeSalary рублей"
+    )
 }
