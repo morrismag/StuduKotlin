33a1cc2ac8d141b161415c477e7b12a651f58a38
morrismag <morrissmag@gmail.com>
2023-05-12 13:59:21 +0300

    KS-3-4 Chess

---
+++ src/main/kotlin/lesson3/lesson_3_task_4.kt
@@ -0,0 +1,16 @@
+package lesson3
+
+// ... Позвольте, товарищ, у меня все ходы записаны. — Контора пишет! ...
+fun main() {
+    var gaitNumber = 1
+    var fromWhereGait = "E2"
+    var whereToGait = "E4"
+
+    println("[$fromWhereGait - $whereToGait ; ход №$gaitNumber]")
+
+    gaitNumber = 2
+    fromWhereGait = "D2"
+    whereToGait = "D3"
+
+    println("[$fromWhereGait - $whereToGait ; ход №$gaitNumber]")
+}
