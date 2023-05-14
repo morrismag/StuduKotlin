1c7516a46eb80903ea6556f935bf29e899472ab0
morrismag <morrissmag@gmail.com>
2023-05-14 12:26:15 +0300

    KS-5-4 Authorisation

---
+++ src/main/kotlin/lesson5/lesson_5_task_4.kt
@@ -0,0 +1,15 @@
+package lesson5
+// ... Ах какой же я придумал пароль ...
+fun main() {
+    println("Приветствуем Вас пользователь. \n Введите логин:")
+    val loginU = readln()
+    if (loginU == LOGIN_USER) {
+        println("Введите пароль")
+        val paswordU = readln()
+            if (paswordU == PASSWORD_USER) println("Мы рады вас снова видеть $loginU.")
+            else println("Неправильно введен пароль. Попробуйте еще раз.")
+    } else println("Такой пользователь не зарегистрирован. Пройдите, пожалуйста, регистрацию.")
+}
+
+const val LOGIN_USER = "Batman"
+const val PASSWORD_USER = "password"
