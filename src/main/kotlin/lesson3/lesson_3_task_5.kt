f9717a03fd54ba5fb13a80d9a38fad61874ed4ce
morrismag <morrissmag@gmail.com>
2023-05-14 11:24:19 +0300

    KS-3-5 Parsing

--- src/main/kotlin/lesson3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson3/lesson_3_task_5.kt
@@ -2,9 +2,9 @@ package lesson3
 
 fun main() {
     val inpitInformation = "D2-D4;0"
-    val fromWhere = inpitInformation.substring(0, 2)
-    val whereTo = inpitInformation.substring(3, 5)
-    val strokeNumber = inpitInformation.substring(6, 7)
+    val fromWhere = inpitInformation.split("-",";").get(0)
+    val whereTo = inpitInformation.split("-",";").get(1)
+    val strokeNumber = inpitInformation.split("-",";").get(2)
 
     println("Откуда $fromWhere")
     println("Куда $whereTo")
