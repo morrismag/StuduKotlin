c9feda7843aca1f71f7d47e35c4c149d60238fad
morrismag <morrissmag@gmail.com>
2023-05-15 09:36:24 +0300

    KS-4-3 Beans

--- src/main/kotlin/lesson4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson4/lesson_4_task_3.kt
@@ -2,17 +2,22 @@ package lesson4
 
 // ... вот же, кроме Губка Боб ни чего и не скажешь ...
 fun main() {
-    val necessaryConditionWeather = "солнечная погода"
-    val necessaryConditionTent = "тент раскрыт"
-    val necessaryConditionHumidity = "влажность 20"
-    val necessaryConditionSeason = "время года не зима"
-    val nowConditionWeather = "солнечная погода"
-    val nowConditionTent = "тент раскрыт"
-    val nowConditionHumidity = "влажность 20"
-    val nowConditionSeason = "время года зима"
+    val isSunOnTheSky = true
+    val isOpenTent = true
+    val itIsBestHumidity = 20
+    val itIsBestSeasons: List<String> = listOf("3","4","5","6","7","8","9","10","11","весна", "лето", "осень")
+    val nowIsSunOnTheSky = true
+    val nowCIsSunOnTheSky = true
+    val nowConditionHumidity = 20
+    val nowConditionSeason = "зима" // в зависимости от переданных данных эту переменную можно преобразовывать
+                                    // выделить например месяц
+                                    //val nowConditionSeason1 = LocalDate.now().monthValue
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
-                "${(necessaryConditionWeather == nowConditionWeather) && (necessaryConditionTent == nowConditionTent) && (necessaryConditionHumidity == nowConditionHumidity) && (necessaryConditionSeason == nowConditionSeason)}"
+                "${(isSunOnTheSky == nowIsSunOnTheSky) 
+                && (isOpenTent == nowCIsSunOnTheSky) 
+                && (itIsBestHumidity == nowConditionHumidity) 
+                && (nowConditionSeason in itIsBestSeasons)}"
     )
 }
