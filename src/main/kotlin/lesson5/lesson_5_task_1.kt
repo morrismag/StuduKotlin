59649cce44255a09e3523e022cc8a25a6bcc65d3
morrismag <morrissmag@gmail.com>
2023-05-15 11:17:06 +0300

    KS-5-1 You won't pass, bot

--- src/main/kotlin/lesson5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson5/lesson_5_task_1.kt
@@ -1,10 +1,17 @@
 package lesson5
+
 // ... нажми на кнопку - получишь результат, и твоя мечта осуществится ...
 fun main() {
+    val firstNum = (1..1000).random()
+    val secondNum = (1..100).random()
+    val sumNum = firstNum + secondNum
 
-    println("Если ты не бот, напиши ответ:\n4+87 =")
+    println(
+        "Если ты не бот, напиши ответ:\n" +
+                "$firstNum + $secondNum ="
+    )
 
     val result = readln()
-        if (result == "91") println("Добро пожаловать!")
+    if (result == sumNum.toString()) println("Добро пожаловать!")
         else println("Доступ запрещен!!!")
 }
