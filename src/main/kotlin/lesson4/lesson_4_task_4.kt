6575ce3a8cb9d82ead6f505ee8cb65bcf3a968fe
morrismag <morrissmag@gmail.com>
2023-05-16 05:14:31 +0300

    KS-4-4 Physical culture

--- src/main/kotlin/lesson4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson4/lesson_4_task_4.kt
@@ -3,14 +3,14 @@ package lesson4
 
 fun main() {
     val numberOfTrainingDay = 8
-    val parityOfDay: Boolean = (numberOfTrainingDay % 2 ==0)
+    val parityOfDay: Boolean = (numberOfTrainingDay % 2 == 0)
     val handsPart: Boolean = !parityOfDay
     val legsPart: Boolean = parityOfDay
     val backPart: Boolean = parityOfDay
     val abdominalPressPart: Boolean = !parityOfDay
 
     println(
-                "Упражнения для рук:    $handsPart\n" +
+        "Упражнения для рук:    $handsPart\n" +
                 "Упражнения для ног:    $legsPart\n" +
                 "Упражнения для спины:  $backPart\n" +
                 "Упражнения для пресса: $abdominalPressPart"
