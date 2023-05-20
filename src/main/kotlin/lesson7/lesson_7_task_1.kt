9309d62a7d49acad800bf514112dc00bbea14a8d
morrismag <morrissmag@gmail.com>
2023-05-20 09:53:21 +0300

    KS-7-1 Generate password

--- src/main/kotlin/lesson7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson7/lesson_7_task_1.kt
@@ -1,22 +1,15 @@
 package lesson7
 
 fun main() {
-    // по условию нужно каждой строке ниже присвоить переменную а потом в строке их вывести,
-    // этот метод короче, но не масштабируемыйю
-   /*  println("${(1..9).random()}" +
-            "${('a'..'z').random()}" +
-            "${(1..9).random()}" +
-            "${('a'..'z').random()}" +
-            "${(1..9).random()}" +
-            "${('a'..'z').random()}") */
 
-    // ниже код мне нравится
+    // как сказал бизнес
 
-    val numS = (1..9)
-    val charS = ('a'..'z')
-    println("${numS.random()}${charS.random()}" +
-            "${numS.random()}${charS.random()}" +
-            "${numS.random()}${charS.random()}")
-
+    val number1 = (1..9).random().toString()
+    val number2 = (1..9).random().toString()
+    val number3 = (1..9).random().toString()
+    val char1 = ('a'..'z').random()
+    val char2 = ('a'..'z').random()
+    val char3 = ('a'..'z').random()
+    println(number1 + char1 + number2 + char2 + number3 + char3)
 
 }
