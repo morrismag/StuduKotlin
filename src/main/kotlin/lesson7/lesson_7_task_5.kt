7f73568a57546e98faaf7ac8ff083cae02ecab63
morrismag <morrissmag@gmail.com>
2023-05-20 20:17:07 +0300

    KS-7-5 Hard password generate

--- src/main/kotlin/lesson7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson7/lesson_7_task_5.kt
@@ -2,13 +2,15 @@ package lesson7
 
 // ... интересная задачка ...
 fun main() {
-    val randomChars = ('1'..'9') + ('a'..'z') + ('A'..'Z').random()
+    val randomChars = ('1'..'9') + ('a'..'z')+('A'..'Z')
     println("Введи число от 1 до 9. Такой длинны будет твой пароль:")
-    var lengthPassword = readln().toIntOrNull()
+    var lengthPassword: Int? = readln().toIntOrNull() ?: 0
+
     while (lengthPassword !in (1..9)) {
         println("Для особенных повторяю:\n\"ВВЕДИ ЧИСЛО от 1 до 9\". Попробуй снова:")
-        lengthPassword = readln().toIntOrNull()
+        lengthPassword = readln().toIntOrNull() ?: 0
     }
-    val resultPassword = (1..lengthPassword!!).map { randomChars.random() }.joinToString("")
+
+    val resultPassword = (1..(lengthPassword?.toInt() ?: 0)).map { randomChars.random() }.joinToString("")
     println("Вот тебе пароль:\n $resultPassword")
 }
