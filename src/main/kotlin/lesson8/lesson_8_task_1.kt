e6558809dae91ecb9e661d7a2a709bed6ab48f9b
morrismag <morrissmag@gmail.com>
2023-05-20 10:17:58 +0300

    KS-8-1 Sum view per week

---
+++ src/main/kotlin/lesson8/lesson_8_task_1.kt
@@ -0,0 +1,7 @@
+package lesson8
+
+fun main() {
+    val viewsPerWeek: IntArray = intArrayOf(5, 7, 1, 8, 4, 1, 2)
+    val sum = viewsPerWeek.sum()
+    println("Сумма просмотров за неделю: $sum")
+}
