246b40256bef71a0c797defd0610a20d6602b329
morrismag <morrissmag@gmail.com>
2023-05-20 18:48:37 +0300

    KS-7-1 Generate password

--- src/main/kotlin/lesson7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson7/lesson_7_task_1.kt
@@ -4,12 +4,12 @@ fun main() {
 
     // как сказал бизнес
 
-    val number1 = (1..9).random().toString()
-    val number2 = (1..9).random().toString()
-    val number3 = (1..9).random().toString()
+    val number1 = (1..9).random()
+    val number2 = (1..9).random()
+    val number3 = (1..9).random()
     val char1 = ('a'..'z').random()
     val char2 = ('a'..'z').random()
     val char3 = ('a'..'z').random()
-    println(number1 + char1 + number2 + char2 + number3 + char3)
+    println("$number1$char1$number2$char2$number3$char3")
 
 }
