6e648ab4ff713953c74e1b1f862fe649f024db74
morrismag <morrissmag@gmail.com>
2023-05-20 10:00:19 +0300

    KS-7-3 Progression

--- src/main/kotlin/lesson7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson7/lesson_7_task_3.kt
@@ -3,7 +3,11 @@ package lesson7
 // ... ни чего не приходит в голову ...
 fun main() {
     println("Введите число от 2 до 20:")
-    val numEndProgression = readln().toInt()
-    val progressionResult = 0..numEndProgression step (2)
+    var numEndProgression = readln().toIntOrNull()
+    while (numEndProgression !in (2..20)) {
+        println("Для особенных повторяю:\n\"ВВЕДИ ЧИСЛО от 2 до 20\". Попробуй снова:")
+        numEndProgression = readln().toIntOrNull()
+    }
+    val progressionResult = 2..numEndProgression!! step 2
     for (i in progressionResult) println("Число четной прогрессии $i")
 }
