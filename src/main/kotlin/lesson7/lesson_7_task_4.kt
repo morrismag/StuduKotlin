08060f31158bb38ca6657c6ca5e57319c019eb7f
morrismag <morrissmag@gmail.com>
2023-05-20 20:20:21 +0300

    KS-7-4 Countdown

--- src/main/kotlin/lesson7/lesson_7_task_4.kt
+++ src/main/kotlin/lesson7/lesson_7_task_4.kt
@@ -1,19 +1,20 @@
 package lesson7
 
-// ... на сегоднешнем уровне я пока не могу вменяемую обработку ошибок написать, если введен текст или ничего,
-// если не буду ипользовать !!...
+// ... получилось...
 fun main() {
     println("Введите кол-во секунд от 1 до 20:")
-    var secondQuantity = readln().toInt()
+    var secondQuantity: Int? = readln().toIntOrNull() ?: 0
 
     while (secondQuantity !in (1..20)) {
         println("Ну тебя же попросили ввести число от 1 до 20. Попробуй снова:")
-        secondQuantity = readln().toInt()
+        secondQuantity = readln().toIntOrNull() ?: 0
     }
 
-    for (i in secondQuantity downTo 1) {
-        println("Осталось $i секунд.")
-        Thread.sleep(1000)
+    if (secondQuantity != null) {
+        for (i in secondQuantity downTo 1) {
+            println("Осталось $i секунд.")
+            Thread.sleep(1000)
+        }
     }
     println("Время вышло. БУМ!!!")
 }
