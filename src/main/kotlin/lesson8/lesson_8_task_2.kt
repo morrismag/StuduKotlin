6eaf60fdcc8b1a85b0ab51aaf034919ee9136e09
morrismag <morrissmag@gmail.com>
2023-05-22 11:31:50 +0300

    KS-8-2 Culinary recipe

--- src/main/kotlin/lesson8/lesson_8_task_2.kt
+++ src/main/kotlin/lesson8/lesson_8_task_2.kt
@@ -2,14 +2,20 @@ package lesson8
 
 // ... признаюсь меня эта задача ввела в глубокие размышления
 // Вариант, который я сделал, не масштабируемый...
+
 fun main() {
     val ingredientsList: Array<String> = arrayOf("соль", "варенье", "лавровый лист", "пельмени", "кетчуп")
-    println("Какой ингридиент Вас интересует:")
+    println("Какой ингредиент Вас интересует:")
     val userIngr = readln()
-    val resultArray: BooleanArray = booleanArrayOf(false, false, false, false, false)
+    var flag = false
+
     for (i in ingredientsList.indices) {
-        resultArray[i] = (userIngr == ingredientsList[i])
+        if (userIngr == ingredientsList[i]) {
+            println("Ингредиент \"$userIngr\" в рецепте есть.")
+            flag = true
+            break
+        }
     }
-    if (true in resultArray) println("Ингридиент \"$userIngr\" в рецепте есть")
-    else println("Такого ингридиента в рецепте нет")
+
+    if (!flag) println("Такого ингредиента \"$userIngr\" в рецепте нет")
 }
