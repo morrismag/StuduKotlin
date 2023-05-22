85b159abeffdf04c3765d5d3e309904dc7ae77ee
morrismag <morrissmag@gmail.com>
2023-05-22 11:51:36 +0300

    KS-8-4 Orchestra

--- src/main/kotlin/lesson8/lesson_8_task_4.kt
+++ src/main/kotlin/lesson8/lesson_8_task_4.kt
@@ -5,23 +5,24 @@ import java.util.*
 // ... это было интересно ...
 fun main() {
     val musicInstruments: Array<String> = arrayOf("Фортепьяно", "Скрипка", "Кларнет", "Труба", "Контрабас")
-
     println("Инструменты, участвующие в оркестре:")
+
     for (i in musicInstruments.indices) {
         println(musicInstruments[i])
     }
-    println("Выбирите, какой инструмент Вы замените:")
-    var userChoice = readln()
 
-    while (userChoice.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-        !in musicInstruments) {
+    println("Выбирите, какой инструмент Вы замените:")
+    var userChoice = readln().lowercase()
+    val musicLower = musicInstruments.map { it.lowercase() }
+
+    while (userChoice !in musicLower) {
         println("Такого инструмента нет в оркестре. Введите инструмент из списка снова:")
         userChoice = readln()
     }
+
     userChoice =
         userChoice.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
     val indexInstrument = musicInstruments.indexOf(userChoice)
-
     println("На какой инструмент вы хотите заменить выбранный " +
             "\"${
                 userChoice.replaceFirstChar
