28f6684bd1697acbbed6cbebf5106e8179fb4820
morrismag <morrissmag@gmail.com>
2023-05-22 12:13:04 +0300

    KS-8-5 Create array

--- src/main/kotlin/lesson8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson8/lesson_8_task_5.kt
@@ -1,25 +1,24 @@
 package lesson8
 
-// ... признаюсь, многое мне помог компилятор ...
 fun main() {
     println("Введи число ингридиентов. До 5 иначе много печатать:")
-    var quantityIngredients = readln().toIntOrNull()
+    var quantityIngredients = readln().toIntOrNull() ?: 0
+
     while (quantityIngredients !in (1..5)) {
         println("Ну тебя же попросили ввести число от 1 до 5. Попробуй снова:")
-        quantityIngredients = readln().toIntOrNull()
+        quantityIngredients = readln().toIntOrNull() ?: 0
     }
 
-    val arrayIngredients = quantityIngredients?.let { arrayOfNulls<String>(it) }
+    val arrayIngredients = arrayOfNulls<String>(quantityIngredients)
     var userIngredient: String
-    for (i in 1..quantityIngredients!!) {
+
+    for (i in 1..quantityIngredients) {
         println("Введите $i-й ингридиент: ")
         userIngredient = readln()
-        arrayIngredients?.set(i - 1, userIngredient)
+        arrayIngredients[i - 1] = userIngredient
     }
-    if (arrayIngredients != null) {
-        println(
-            "Вот что получилось:" +
-                    "[${arrayIngredients.joinToString("; ")}]"
-        )
-    }
+
+    println(
+        "Вот что получилось: [${arrayIngredients.joinToString(", ")}]"
+    )
 }
