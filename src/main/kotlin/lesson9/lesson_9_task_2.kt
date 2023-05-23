24b5cef64d609d920dc670c45140d78c5f10c27d
morrismag <morrissmag@gmail.com>
2023-05-23 11:18:17 +0300

    KS-9-2 Add ingredient

--- src/main/kotlin/lesson9/lesson_9_task_2.kt
+++ src/main/kotlin/lesson9/lesson_9_task_2.kt
@@ -1,7 +1,5 @@
 package lesson9
 
-import kotlin.system.exitProcess
-
 fun main() {
     val ingredientsList = mutableListOf("Авокадо", "Манго", "Кокос")
     println("В рецепте есть базовые ингредиенты:")
@@ -13,14 +11,14 @@ fun main() {
     println("Желаете добавить еще? (ответ да/нет)")
     val answer = readln()
 
-    if (answer != "да") exitProcess(0)
-    else println("Какой ингредиент вы хотите добавить?")
+    if (answer == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val addIngredient = readln()
+        ingredientsList.add(addIngredient)
+        println("Теперь в рецепте есть следующие ингредиенты:")
 
-    val addIngredient = readln()
-    ingredientsList.add(addIngredient)
-    println("Теперь в рецепте есть следующие ингредиенты:")
-
-    ingredientsList.forEach {
-        println(it)
+        ingredientsList.forEach {
+            println(it)
+        }
     }
 }
