7554378057a87865904b600b73ce03d6c50fddde
morrismag <morrissmag@gmail.com>
2023-05-23 06:18:47 +0300

    KS-10-5 Full authorization

---
+++ src/main/kotlin/lesson10/lesson_10_task_5.kt
@@ -0,0 +1,83 @@
+package lesson10
+
+fun main() {
+    println("Для регистрации придумайте логин не менее 4-х символов:")
+    var loginUser = readln()
+
+    while (checkEnter(loginUser) == 0){
+        loginUser = readln()
+    }
+
+    val passwordUser = generationPassword(8)
+    println("Для вас сгенерирован новый пароль:$passwordUser")
+    println("\nАвторизуйтесь пожалуйста.\nВведите логин:")
+    var loginUserEnter = readln()
+    println("Введите пароль:")
+    var passwordUserEnter = readln()
+
+    while (checkLoginPasswordEnter(loginUser, passwordUser, loginUserEnter, passwordUserEnter) == 0){
+        println("Введите снова логин:")
+        loginUserEnter = readln()
+        println("Введите снова пароль:")
+        passwordUserEnter = readln()
+    }
+
+    var codeAuthorization = (1000..9999).random().toString()
+    println("Вам выслан код авторизации: $codeAuthorization")
+    println("Введите код авторизации:")
+    var codeAuthorizationEnter = readln()
+
+    while (checkCode(codeAuthorization,codeAuthorizationEnter) == 0){
+        codeAuthorization =(1000..9999).random().toString()
+        println("Вам повторно выслан код авторизации: $codeAuthorization")
+        codeAuthorizationEnter = readln()
+    }
+
+}
+
+fun checkEnter(enter: String): Int {
+    val result = if (enter.length >= 4) {
+        println("Логин принят.")
+        1
+    } else {
+        println("Значение меньше 4-х символов. Введите снова:")
+        0
+    }
+    return result
+}
+
+fun generationPassword(numberOfPassword: Int): String {
+    val numPart: MutableList<Char> = ("123456789").toMutableList()
+    val randomChars = ('1'..'9') + ('a'..'z') + ('A'..'Z') +
+            '!' + '"' + '#' + '$' + '%' + '&' + '\'' + '(' + ')' + '*' + '+' + ',' + '-' + '.' + '/'
+    val resultPassword: Array<String> = Array(numberOfPassword) { "" }
+
+    for (i in 0 until numberOfPassword) {
+        if ((i % 2) != 0) {
+            resultPassword[i] = randomChars.random().toString()
+        } else resultPassword[i] = numPart.random().toString()
+    }
+
+    return resultPassword.joinToString("")
+}
+
+fun checkLoginPasswordEnter(login: String, password:String, loginEnter: String, passwordEnter: String): Int {
+    val result = if ((login == loginEnter) && (password==passwordEnter)) {
+        println("Авторизация пройдена успешно!")
+        1
+    } else {
+        println("Вы неправильно ввели или логин или пароль. Введите снова:")
+        0
+    }
+    return result
+}
+
+fun checkCode(code: String, codeEnter:String): Int {
+    val result = if (code == codeEnter) {
+        println("Ура. Вы наконец авторизовались!!!")
+        1
+    } else {
+        0
+    }
+    return result
+}
