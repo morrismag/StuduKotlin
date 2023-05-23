d9defc87287ecf9ea62c2b62ebf7db66065913f3
morrismag <morrissmag@gmail.com>
2023-05-23 11:45:15 +0300

    KS-9-4 Sort

--- src/main/kotlin/lesson9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson9/lesson_9_task_4.kt
@@ -1,9 +1,8 @@
 package lesson9
- // ... намучился с функцией sort ...
+
 fun main() {
     print("Введите, пожалуйста, пять ингридиентов через запятую:\n")
     val stringIngredients = readln()
-    val listIngredients = stringIngredients.split(",").toMutableList()
-    listIngredients.sort()
-    println(listIngredients)
+    val listIngredients: List<String> = stringIngredients.split(",")
+     println(listIngredients.sorted())
 }
