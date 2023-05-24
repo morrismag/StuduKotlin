075425d02d3ef3bf33ab3c7a4145e2aeb4bad6a6
morrismag <morrissmag@gmail.com>
2023-05-24 11:43:04 +0300

    KS-8-5 Create array

--- src/main/kotlin/lesson8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson8/lesson_8_task_5.kt
@@ -1,6 +1,13 @@
 package lesson8
 
 fun main() {
+    val arrayIngr = Array(createArray()) { readln() }
+    println(
+        "Вот что получилось: [${arrayIngr.joinToString(", ")}]"
+    )
+}
+
+fun createArray(): Int{
     println("Введи число ингридиентов. До 5 иначе много печатать:")
     var quantityIngredients = readln().toIntOrNull() ?: 0
 
@@ -8,17 +15,6 @@ fun main() {
         println("Ну тебя же попросили ввести число от 1 до 5. Попробуй снова:")
         quantityIngredients = readln().toIntOrNull() ?: 0
     }
-
-    val arrayIngredients = arrayOfNulls<String>(quantityIngredients)
-    var userIngredient: String
-
-    for (i in 1..quantityIngredients) {
-        println("Введите $i-й ингридиент: ")
-        userIngredient = readln()
-        arrayIngredients[i - 1] = userIngredient
-    }
-
-    println(
-        "Вот что получилось: [${arrayIngredients.joinToString(", ")}]"
-    )
+    println("Введи $quantityIngredients ингредиентов. Отделяй ингредиенты Enter:")
+    return quantityIngredients
 }
