b345ac370777114cacba423a2318ef1ac6a3593d
morrismag <morrissmag@gmail.com>
2023-05-24 14:03:28 +0300

    KS-10-4 Hard game

--- src/main/kotlin/lesson10/lesson_10_task_4.kt
+++ src/main/kotlin/lesson10/lesson_10_task_4.kt
@@ -8,47 +8,39 @@ fun main() {
         val userPlay = random(1)
         println("Ход компьютера: $computerPlay")
         println("Ход игрока: $userPlay")
+        val resultWin = selectTheResult(computerPlay, userPlay)
 
-        val resultWin = selectingTheResult(computerPlay, userPlay)
-
-        if (resultWin == 1) sumWin++
+        if (resultWin) sumWin++
 
         println("Хотите ли еще раз поиграть? (ответ да/нет)")
         val answer = readln()
-
-        val answerUser = when (answer) {
-            "да" -> true
-            else -> false
-        }
+        val answerUser = answer == "да"
     } while (answerUser)
 
     println("Вы победили всего: $sumWin")
 }
 
-fun random(q: Int): Int {
+fun random(playerId: Int): Int {
     val computer = (1..6).random()
     val user = (1..6).random()
 
-    return if (q == 0) computer
+    return if (playerId == 0) computer
     else user
 }
 
-fun selectingTheResult(computerPlay: Int, userPlay: Int): Int {
-    val resultWin = when {
-        computerPlay > userPlay -> {
-            println("Компьютер выйграл.")
-            0
+fun selectTheResult(computerPlay: Int, userPlay: Int): Boolean {
+    when {
+        computerPlay < userPlay -> {
+            println("Человек выйграл")
         }
 
-        computerPlay < userPlay -> {
-            println("Человек выйграл.")
-            1
+        computerPlay > userPlay -> {
+            println("Компьютер выйграл")
         }
 
         else -> {
-            println("Победила дружба!")
-            0
+            println("Ничья")
         }
     }
-    return resultWin
+    return computerPlay < userPlay
 }
