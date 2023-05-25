133e27b19466a9d932d4b46136593c6c00189f0e
morrismag <morrissmag@gmail.com>
2023-05-25 10:12:16 +0300

    KS-9-5 New list ingredients

--- src/main/kotlin/lesson9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson9/lesson_9_task_5.kt
@@ -1,26 +1,20 @@
 package lesson9
 
-// ... Иван у меня куча вопросов:
-// 1. Когда я присваиваю MutableList другой переменной он становится другого типа? Перестают работать
-// функции sort и т.д
-// 2.какой функцией можно заменить capitalize оптимально, как я сделал или, что то есть покороче?
-
 fun main() {
-    println("Делаем рецепт:\nВведи первый ингридиент:")
-    val firstIngr = readln()
-    println("Введи второй ингридиент:")
-    val secondIngr = readln()
-    println("Введи третий ингридиент:")
-    val thirdIngr = readln()
-    println("Введи четвертый ингридиент:")
-    val fourthIngr = readln()
-    println("Введи пятый ингридиент:")
-    val fifthIngr = readln()
-    val listIngr: MutableList<String> = mutableListOf(firstIngr, secondIngr, thirdIngr, fourthIngr, fifthIngr)
-    val distinctList = listIngr.toSet().toMutableList()
-    distinctList.sort()
-    distinctList[0] = distinctList[0].replaceFirstChar { it.uppercaseChar() }
-    println(distinctList[0])
-    val strList = distinctList.joinToString(",")
-    println("Мы получили следующий список ингридиентов: $strList.")
+    println("Делаем рецепт.\n Введи пять ингридиентов через Enter:")
+    val listIngr = MutableList(5) { checkCreateList() }
+    val newList = listIngr.toSet().sorted().toMutableList()
+    newList[0] = newList[0].replaceFirstChar { it.uppercaseChar() }
+    newList[newList.size - 1] = newList[newList.size - 1] + "."
+    println("Мы получили следующий список ингридиентов: ${newList.joinToString(",")}")
+}
+
+fun checkCreateList(): String {
+    var nameIngredients = readln()
+
+    while (nameIngredients == "") {
+        println("Ты не ввел ингредиент. Попробуй снова:")
+        nameIngredients = readln()
+    }
+    return nameIngredients
 }
