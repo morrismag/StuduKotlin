e8743a9ac37327ae4f63022b9c98d2ce4ca84dee
morrismag <morrissmag@gmail.com>
2023-05-25 11:19:26 +0300

    KS-12-3 Weather main simplified recording

---
+++ src/main/kotlin/lesson12/lesson_12_task_3.kt
@@ -0,0 +1,52 @@
+package lesson12
+
+class Day(
+    val idDay:Int,
+    val nameDay: String,
+    val temperatureDay: String,
+    val temperatureNight:String,
+    val atmosphericPressure: Int,
+    val rainIs: Boolean = false,
+){
+
+    fun readInformation() {
+        println("Информация о погоде\n$idDay")
+        println(nameDay)
+        println(temperatureDay)
+        println(temperatureNight)
+        println(rainIs)
+        println(atmosphericPressure)
+    }
+
+}
+fun main() {
+    val mondayWeather =
+        Day(1,
+            "Понедельник",
+            "24",
+            "12",
+            730)
+
+
+    val tuesdayWeather =
+        Day(2,
+            "Вторник",
+            "22",
+            "14",
+            720,
+            true)
+
+    val wednesdayWeather =
+        Day(3,
+            "Среда",
+            "30",
+            "10",
+            725)
+
+    mondayWeather.readInformation()
+    println()
+    tuesdayWeather.readInformation()
+    println()
+    wednesdayWeather.readInformation()
+
+}
