354541417a2f1b3b44c67fa49b40f1613e825556
morrismag <morrissmag@gmail.com>
2023-05-26 13:07:41 +0300

    KS-12-1 Weather

--- src/main/kotlin/lesson12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson12/lesson_12_task_1.kt
@@ -1,12 +1,12 @@
 package lesson12
 
-class Weather(){
-    var idDay =0
-    var nameDay =""
-    var temperatureDay=""
-    var temperatureNight=""
+class Weather {
+    var idDay = 0
+    var nameDay = ""
+    var temperatureDay = ""
+    var temperatureNight = ""
     var rainIs = false
-    var atmosphericPressure=0
+    var atmosphericPressure = 0
     fun readInformation() {
         println("Информация о погоде\n$idDay")
         println(nameDay)
@@ -15,24 +15,24 @@ class Weather(){
         println(rainIs)
         println(atmosphericPressure)
     }
-
 }
+
 fun main() {
     val mondayWeather = Weather()
-    mondayWeather.idDay=1
-    mondayWeather.nameDay="Понедельник"
-    mondayWeather.temperatureDay="12"
-    mondayWeather.temperatureNight="10"
-    mondayWeather.rainIs=true
-    mondayWeather.atmosphericPressure=730
+    mondayWeather.idDay = 1
+    mondayWeather.nameDay = "Понедельник"
+    mondayWeather.temperatureDay = "12"
+    mondayWeather.temperatureNight = "10"
+    mondayWeather.rainIs = true
+    mondayWeather.atmosphericPressure = 730
 
     val tuesdayWeather = Weather()
-    tuesdayWeather.idDay=2
-    tuesdayWeather.nameDay="Вторник"
-    tuesdayWeather.temperatureDay="22"
-    tuesdayWeather.temperatureNight="15"
-    tuesdayWeather.rainIs=false
-    tuesdayWeather.atmosphericPressure=715
+    tuesdayWeather.idDay = 2
+    tuesdayWeather.nameDay = "Вторник"
+    tuesdayWeather.temperatureDay = "22"
+    tuesdayWeather.temperatureNight = "15"
+    tuesdayWeather.rainIs = false
+    tuesdayWeather.atmosphericPressure = 715
 
     mondayWeather.readInformation()
     println()
