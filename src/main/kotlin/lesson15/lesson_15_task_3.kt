45140cf5afae49467c2b5bc1a27e5212116111e2
morrismag <morrissmag@gmail.com>
2023-05-28 15:21:33 +0300

    KS-15-3 Admin and user

---
+++ src/main/kotlin/lesson15/lesson_15_task_3.kt
@@ -0,0 +1,50 @@
+package lesson15
+
+abstract class AllUsers {
+    abstract val user: String
+
+    abstract fun readMassage()
+    abstract fun writeMassage()
+}
+
+class User(override val user: String) : AllUsers() {
+
+    override fun readMassage() {
+        println("Я $user читаю сообщения")
+    }
+
+    override fun writeMassage() {
+        println("Я $user пишу сообщения")
+    }
+}
+
+class Administrator(override val user: String) : AllUsers() {
+    override fun readMassage() {
+        println("Я $user читаю сообщения")
+    }
+
+    override fun writeMassage() {
+        println("Я $user пишу сообщения")
+    }
+
+    fun deleteUser() {
+        println("Я $user удаляю всяких пользоватетей")
+    }
+
+    fun deleteMassage() {
+        println("Я $user удаляю всякие мусорные сообщения")
+    }
+}
+
+fun main() {
+    val userInChat = User("пользователь")
+    val administratorChat = Administrator("админ")
+
+    userInChat.readMassage()
+    userInChat.writeMassage()
+    println()
+    administratorChat.readMassage()
+    administratorChat.writeMassage()
+    administratorChat.deleteMassage()
+    administratorChat.deleteUser()
+}
