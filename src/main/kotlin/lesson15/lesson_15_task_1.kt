6a5ca075cab56b69450db2321f66a2c7bf818651
morrismag <morrissmag@gmail.com>
2023-05-28 13:39:25 +0300

    KS-15-1 Karas, Utka, Chayka

--- src/main/kotlin/lesson15/lesson_15_task_1.kt
+++ src/main/kotlin/lesson15/lesson_15_task_1.kt
@@ -1,37 +1,38 @@
 package lesson15
 
-interface FlyingCreatures{
+interface FlyingCreatures {
     fun flying()
 }
 
-interface FloatingCreatures{
+interface FloatingCreatures {
     fun floating()
 }
 
 class Karas(
-    val name : String
-): FloatingCreatures {
+    val name: String
+) : FloatingCreatures {
     override fun floating() {
         println("Это существо $name плавает под водой")
     }
 
 }
+
 class Utka(
-    val name : String
-): FloatingCreatures {
+    val name: String
+) : FloatingCreatures {
     override fun floating() {
         println("Это существо $name плавает по поверхности воды")
     }
 
 }
+
 class Chayka(
-    val name : String
-): FlyingCreatures {
+    val name: String
+) : FlyingCreatures {
     override fun flying() {
         println("Это существо $name летает над водой")
     }
 }
-
 
 fun main() {
     val karas = Karas("карась")
@@ -41,5 +42,4 @@ fun main() {
     println("${karas.floating()}")
     println("${utka.floating()}")
     println("${chayka.flying()}")
-
 }
