19550fa94ab86e3d2c9cacd0803d471afab7adb6
morrismag <morrissmag@gmail.com>
2023-05-29 13:50:04 +0300

    KS-16-3 Check password

--- src/main/kotlin/lesson16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson16/lesson_16_task_3.kt
@@ -1,18 +1,22 @@
 package lesson16
 
-class User {
-    val login: String = "Max"
-    private val password: String = "qwe"
-
-    fun check(inputPassword: String) {
-        if (inputPassword == password) println("Пароль верный")
-        else println("Увы. В другой раз повезет.")
+class User(
+    val login: String,
+    private val password: String
+) {
+    fun check(inputPassword: String): Boolean {
+        return inputPassword == password
     }
 }
 
 fun main() {
-    val user = User()
+    val user = User(
+        "Max",
+        "qwe"
+    )
     println("Если ты ${user.login} введи пароль:")
     val inputPassword = readln()
-    user.check(inputPassword)
+
+    if (user.check(inputPassword)) println("Пароль верный")
+    else println("Увы. В другой раз повезет.")
 }
