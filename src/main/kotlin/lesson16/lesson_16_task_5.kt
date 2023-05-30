9b575f782ec158285a5f8ad5f5fcf098f01ba900
morrismag <morrissmag@gmail.com>
2023-05-30 13:47:21 +0300

    KS-16-5 Valera in game

--- src/main/kotlin/lesson16/lesson_16_task_5.kt
+++ src/main/kotlin/lesson16/lesson_16_task_5.kt
@@ -1,48 +1,57 @@
 package lesson16
 
-class  Gamer(    val nameGamer: String,        // имя игрока
-         private var health: Int,              // здоровье игрока
-                 var impactForce: Int,        // сила удара игрока
-                 var damage: Int,             // урон, нанесенный игроку
-                 var takeHealth:Int,          // хилинг
-    )
-{
+class Gamer(
+    val nameGamer: String,        // имя игрока
+    private var health: Int,              // здоровье игрока
+    var impactForce: Int,        // сила удара игрока
+    var damage: Int,             // урон, нанесенный игроку
+    var takeHealth: Int,          // хилинг
+) {
 
-        fun startGame(){
-            println("Начало игры!")
-            println("Игрок: $nameGamer\n" +
+    fun startGame() {
+        println("Начало игры!")
+        println(
+            "Игрок: $nameGamer\n" +
                     "Здоровье: $health\n" +
-                    "Сила удара:$impactForce")
-        }
-    fun takeDamage(){
+                    "Сила удара:$impactForce"
+        )
+    }
+
+    fun takeDamage() {
         println("Ты нанес удар Игроку $nameGamer равный $damage hp")
         health -= damage
-        if (health<= 0) {
+        if (health <= 0) {
             deathGamer()
             takeHealth = 0
-        }
-        else println("Игрок: $nameGamer\n" +
-                "Здоровье: $health\n" +
-                "Сила удара:$impactForce")
+        } else println(
+            "Игрок: $nameGamer\n" +
+                    "Здоровье: $health\n" +
+                    "Сила удара:$impactForce"
+        )
     }
 
-    fun treatment(){
+    fun treatment() {
         println("Ты подлечил Игрока $nameGamer на $takeHealth hp")
         health += takeHealth
-        println("Игрок: $nameGamer\n" +
-                "Здоровье: $health\n" +
-                "Сила удара:$impactForce")
+        println(
+            "Игрок: $nameGamer\n" +
+                    "Здоровье: $health\n" +
+                    "Сила удара:$impactForce"
+        )
     }
 
-    private fun deathGamer(){
+    private fun deathGamer() {
         impactForce = 0
         health = 0
-        println("Игрок: $nameGamer\n" +
-                "Здоровье: $health\n" +
-                "Сила удара:$impactForce\n" +
-                "И вообще он уже на приеме у апостала Павла)")
-        }
+        println(
+            "Игрок: $nameGamer\n" +
+                    "Здоровье: $health\n" +
+                    "Сила удара:$impactForce\n" +
+                    "И вообще он уже на приеме у апостала Павла)"
+        )
+    }
 }
+
 fun main() {
     val gamer = Gamer(
         "Валера",
@@ -68,5 +77,4 @@ fun main() {
     println()
     Thread.sleep(4000)
     gamer.treatment()
-
 }
