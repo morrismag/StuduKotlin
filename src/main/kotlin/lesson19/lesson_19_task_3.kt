0c569c0d0aa34ea85af68fc3b75bf1ef5834cf7d
morrismag <morrissmag@gmail.com>
2023-05-31 13:20:04 +0300

    KS-19-3 TODO in program

---
+++ src/main/kotlin/lesson19/lesson_19_task_3.kt
@@ -0,0 +1,15 @@
+package lesson19
+
+class SpaceShip{
+    fun takeOff(){}
+    fun landingOn(){
+        //TODO надо прописать разную логику прижемлени на планеты и пристаковки к космическим станция.
+    }
+    fun shootingAtAsteroids(){
+        TODO("NotImplementedError")
+    }
+}
+fun main() {
+  val newSpaceShip = SpaceShip()
+    newSpaceShip.shootingAtAsteroids()
+}
