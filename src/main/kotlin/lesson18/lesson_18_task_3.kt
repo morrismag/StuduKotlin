9062f7d364c7467212638555ee8aa8c21da78b31
morrismag <morrissmag@gmail.com>
2023-06-01 15:37:52 +0300

    KS-18-3 Fox, Cat, Dog

--- src/main/kotlin/lesson18/lesson_18_task_3.kt
+++ src/main/kotlin/lesson18/lesson_18_task_3.kt
@@ -1,34 +1,49 @@
 package lesson18
 
-open class Animal {
-     var nameAnimal: String = ""
-    open fun playAnimal() = ""
-    open fun eatAnimal() = ""
-    open fun sleepAnimal() = ""
-
+abstract class Animal() {
+    var nameAnimal: String = ""
+    abstract fun playAnimal()
+    abstract fun eatAnimal(): String
+    abstract fun sleepAnimal()
 }
 
 class Fox : Animal() {
+    override fun playAnimal() {}
 
     override fun eatAnimal(): String {
         return "Животное $nameAnimal ест ягоды"
     }
+
+    override fun sleepAnimal() {}
 }
-class Cat:Animal(){
+
+class Cat : Animal() {
+    override fun playAnimal() {
+    }
+
     override fun eatAnimal(): String {
         return "Животное $nameAnimal ест рыбу"
     }
+
+    override fun sleepAnimal() {
+    }
 }
 
-class Dog:Animal(){
+class Dog : Animal() {
+    override fun playAnimal() {
+    }
+
     override fun eatAnimal(): String {
         return "Животное $nameAnimal грызет кости"
+    }
+
+    override fun sleepAnimal() {
     }
 }
 
 fun main() {
-    val fox=Fox()
-    fox.nameAnimal ="Лиса Алиса"
+    val fox = Fox()
+    fox.nameAnimal = "Лиса Алиса"
     val cat = Cat()
     cat.nameAnimal = "Кошка"
     val dog = Dog()
