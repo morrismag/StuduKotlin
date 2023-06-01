0616046084cf3a638169b5818f22ead4cbc74926
morrismag <morrissmag@gmail.com>
2023-06-01 15:52:31 +0300

    KS-18-4 Square packed box

--- src/main/kotlin/lesson18/lesson_18_task_4.kt
+++ src/main/kotlin/lesson18/lesson_18_task_4.kt
@@ -3,29 +3,29 @@ package lesson18
 import kotlin.math.pow
 
 abstract class Packed {
-    abstract fun calculateSquare(): Int
-
+    abstract fun calculateSquare(): Double
 }
 
 class RectangleBox(
     val sideA: Int,
     val sideB: Int,
+    val sideC: Int
 ) : Packed() {
-    override fun calculateSquare(): Int {
-        return sideA * sideB
+    override fun calculateSquare(): Double {
+        return (sideA * sideB * sideC).toDouble()
     }
 }
 
 class CubeBox(
     val sideA: Int,
 ) : Packed() {
-    override fun calculateSquare(): Int {
-        return sideA.toDouble().pow(2).toInt()
+    override fun calculateSquare(): Double {
+        return sideA.toDouble().pow(2)
     }
 }
 
 fun main() {
-    val rectanglePack = RectangleBox(2, 4)
+    val rectanglePack = RectangleBox(2, 4,5)
     val cubePack = CubeBox(5)
     val arrayOfPacked = arrayOf(rectanglePack, cubePack)
     showSquare(arrayOfPacked)
