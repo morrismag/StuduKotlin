eb6ea2a9f4fccbd956acfc53ed1a08c1bb120878
morrismag <morrissmag@gmail.com>
2023-06-05 16:02:00 +0300

    KS-20-4 String to Lambda

--- src/main/kotlin/lesson20/lesson_20_task_4.kt
+++ src/main/kotlin/lesson20/lesson_20_task_4.kt
@@ -2,13 +2,7 @@ package lesson20
 
 fun main() {
     val listString = listOf("1", "2", "3", "4", "5", "6", "7")
-    val listLambda = mutableListOf(listString.map {
-        if (listString.indexOf(it) % 2 != 0) {
-            println("Нажат элемент $it")
-        } else {
-            println(it)
-        }
-    }
-    )
-    listLambda.forEach { _ -> }
+    val listLambda = listString.map { { println("Нажат элемент $it") } }
+
+    for (i in listLambda.indices) if (i % 2 != 0) listLambda[i].invoke()
 }
