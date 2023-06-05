f526a920eb8ab2c3ce973709377bbeafdce54f19
morrismag <morrissmag@gmail.com>
2023-06-05 12:52:57 +0300

    KS-19-4 Best Tank shot

--- src/main/kotlin/lesson19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson19/lesson_19_task_4.kt
@@ -1,7 +1,7 @@
 package lesson19
 
 class Tank {
-    fun reloadingWeapons(): String {
+    fun reloadWeapons(): String {
         println(
             "Стрелок поменять тип патронов. Выбери:\n" +
                     "синие, зеленые, красные."
@@ -11,9 +11,9 @@ class Tank {
 
     fun shotTank(typePatron: String): String {
         return when (typePatron) {
-            "синие" -> "Нанесенный урон: ${TypePatron.BLUE.shotPower()}"
-            "зеленые" -> "Нанесенный урон: ${TypePatron.GREEN.shotPower()}"
-            "красные" -> "Нанесенный урон: ${TypePatron.RED.shotPower()}"
+            "синие" -> "Нанесенный урон: ${TypePatron.BLUE.damage}"
+            "зеленые" -> "Нанесенный урон: ${TypePatron.GREEN.damage}"
+            "красные" -> "Нанесенный урон: ${TypePatron.RED.damage}"
             else -> "Мехвод вывози нас отсюда, этот косорукий не может нормально перезарядить пушку!!!"
         }
     }
@@ -21,31 +21,17 @@ class Tank {
 
 fun main() {
     val tankT90 = Tank()
-    var typePatron = tankT90.reloadingWeapons()
+    var typePatron = tankT90.reloadWeapons()
     println()
     println(tankT90.shotTank(typePatron))
     println()
-    typePatron = tankT90.reloadingWeapons()
+    typePatron = tankT90.reloadWeapons()
     println()
     println(tankT90.shotTank(typePatron))
 }
 
-enum class TypePatron {
-    BLUE {
-        override fun shotPower(): Int {
-            return 5
-        }
-    },
-    GREEN {
-        override fun shotPower(): Int {
-            return 10
-        }
-    },
-    RED {
-        override fun shotPower(): Int {
-            return 20
-        }
-    };
-
-    abstract fun shotPower(): Int
+enum class TypePatron(val damage: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20),
 }
