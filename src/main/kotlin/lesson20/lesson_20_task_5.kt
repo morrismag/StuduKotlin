df39f121d8d05e0bdb8053d0d80b6388d0438fa4
morrismag <morrissmag@gmail.com>
2023-06-07 12:38:29 +0300

    KS-20-5 String with help Lambda reverse

--- src/main/kotlin/lesson20/lesson_20_task_5.kt
+++ src/main/kotlin/lesson20/lesson_20_task_5.kt
@@ -1,22 +1,32 @@
 package lesson20
 
-class Robot {
-    fun say(): String {
-        return listOf(
-            "Голос истины противен слуху.",
-            "Кто не борется, тот непобедим.",
-            "Вами управляет тот, кто вас злит.",
-            " Мудрец избегает всякой крайности.",
-            "Человек с самого рождения умирает. "
-        ).random()
+class Robot(
+    val list: List<String> = listOf(
+        "Голос истины противен слуху.",
+        "Кто не борется, тот непобедим.",
+        "Вами управляет тот, кто вас злит.",
+        " Мудрец избегает всякой крайности.",
+        "Человек с самого рождения умирает. "
+    )
+) {
+    val result: String = list.random()
+    fun say(): String? {
+        return if (modifier == null) result
+        else modifier?.invoke(result)
     }
 
-    fun setModifier(say: (String) -> Unit) {
-        return println( say())
+    private var modifier: ((String) -> String)? = null
 
+    fun setModifier(modifierA: (String) -> String) {
+        { modifier = modifierA }
     }
 }
+
 fun main() {
     val robotSay = Robot()
-    println( robotSay.say().reversed() )
+    println(robotSay.say())
+    robotSay.setModifier { it.reversed() }
+    println(robotSay.say())
+
+
 }
